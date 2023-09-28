b66822484805c98ae731ceac8922e8ca27ae603d
Minjae <flower14288@gmail.com>
2023-09-29 02:23:15 +0900

    소년 방향 구현

--- tracing_boy.py
+++ tracing_boy.py
@@ -4,7 +4,8 @@ import random
 open_canvas(1280, 1024)
 
 back = load_image('TUK_GROUND.png')
-character = load_image('left.png')
+left = load_image('left.png')
+right= load_image('right.png')
 hand = load_image('hand_arrow.png')
 
 x1, y1 = 640, 512
@@ -16,14 +17,21 @@ while True:
     points = [random.randint(0, 1280), random.randint(0, 1024)]
     x2, y2 = points[0], points[1]
     hand.draw_now(x2, y2)
+
     # 소년 이동
-    character.draw_now(x1, y1)
     for i in range (0, 100 + 1, 5):
         t = i / 100
         x = (1-t)*x1 + t*x2
         y = (1-t)*y1 + t*y2
         back.draw_now(640, 512)
         hand.draw_now(x2, y2)
-        character.draw_now(x, y)
-        delay(0.1)
+        
+        # 방향
+        if x2 > x1:
+            right.draw_now(x, y)
+        elif x2 < x1:
+            left.draw_now(x, y)
+
+        # 한 칸 이동 시간
+        delay(0.05)
         update_canvas()
