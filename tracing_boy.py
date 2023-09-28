23d8fd12befe593d52e2ffc940eb98d820839940
Minjae <flower14288@gmail.com>
2023-09-29 01:25:29 +0900

    손 이동 개선
    이동하며 손 자국을 남기지 않음

--- tracing_boy.py
+++ tracing_boy.py
@@ -7,12 +7,14 @@ back = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
-back.draw_now(640, 512)
-
+#def going():
+    
 drawing = True
-
 # 랜덤 위치에 손 생성
 while drawing:
+    back.draw_now(640, 512)
     points = [random.randint(0, 1280), random.randint(0, 1024)]
-    delay(3)
     hand.draw_now(points[0], points[1])
+    update_canvas()
+    delay(3)    
+
