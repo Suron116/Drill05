894422bc0f51bb83edb1af1a30e87e0874537320
Minjae <flower14288@gmail.com>
2023-09-29 01:19:51 +0900

    랜덤 위치에 손 생성

--- tracing_boy.py
+++ tracing_boy.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 open_canvas(1280, 1024)
 
@@ -7,3 +8,11 @@ character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
 back.draw_now(640, 512)
+
+drawing = True
+
+# 랜덤 위치에 손 생성
+while drawing:
+    points = [random.randint(0, 1280), random.randint(0, 1024)]
+    delay(3)
+    hand.draw_now(points[0], points[1])
